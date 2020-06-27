6e3eb362473ca032fe93ee5cf81a98e2dd369bdb
edimossilva <edimossilva@gmail.com>
2020-06-28 07:53:38 +1200

    Adding tests to name and surname

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,30 @@
 <template>
 <div>
   <h1>Form</h1>
-  <h2>Preencha seus dados</h2>
+  <h2>Welcome {{fullname}}</h2>
+  <div>
+    <label>Name:</label>
+    <input v-model="name" class="form__label_name" type="text" name="formName">
+  </div>
+  <div>
+    <label>Last Name:</label>
+    <input v-model="lastName" class="form__label_lastname" type="text" name="formLastName">
+  </div>
 </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    name: '',
+    lastName: '',
+  }),
 
+  computed: {
+    fullname() {
+      return `${this.name} ${this.lastName}`;
+    },
+  },
 };
 </script>
 
--- tests/unit/form.spec.js
+++ tests/unit/form.spec.js
@@ -6,8 +6,19 @@ describe('Form', () => {
     const wrapper = shallowMount(Form);
     expect(wrapper.exists()).toBe(true);
   });
+
   it("should has h1 equals to 'Form'", () => {
     const wrapper = shallowMount(Form);
     expect(wrapper.find('h1').text()).toMatch('Form');
+  });
+
+  it("should show 'Welcome {name} {surname}'", async () => {
+    const wrapper = shallowMount(Form);
+    wrapper.find('.form__label_name').setValue('edimo');
+    wrapper.find('.form__label_lastname').setValue('sousa');
+
+    await wrapper.vm.$forceUpdate(); // this makes vue update the DOM
+
+    expect(wrapper.find('h2').text()).toBe('Welcome edimo sousa');
   });
 });
