6deb08d253e5d14e78d4855e1222d87137fb99fa
edimossilva <edimossilva@gmail.com>
2020-06-29 01:49:55 +1200

    add tests to form fields

--- tests/e2e/specs/form.js
+++ tests/e2e/specs/form.js
@@ -1,17 +1,37 @@
 describe('Form', () => {
   describe('When visit form url', () => {
-    it('contains h1 Form', () => {
+    const validName = 'edimo';
+    const validLastName = 'sousa';
+
+    beforeEach(() => {
       cy.visit('/form');
+    });
+
+    it('contains h1 Form', () => {
       cy.contains('h1', 'Form');
     });
 
     describe('When fill name and lastname', () => {
-      it('should got to home When click button', () => {
-        cy.visit('/form');
-        cy.get('.form__input_name').type('edimo');
-        cy.get('.form__input_last_name').type('sousa');
-        cy.get('button').click();
-        cy.url().should('eq', 'http://localhost:8081/'); // => true
+      beforeEach(() => {
+        cy.get('.form__input_name').type(validName);
+        cy.get('.form__input_last_name').type(validLastName);
+      });
+
+      it('should has button', () => {
+        cy.get('button').should('be.visible');
+      });
+
+      it('should has welcome text', () => {
+        cy.get('button').should('be.visible');
+        cy.contains('.form__h2_welcome_message', `Welcome ${validName} ${validLastName}`);
+      });
+
+      describe('When click button', () => {
+        it('should got to home', () => {
+          cy.url().should('eq', 'http://localhost:8080/form'); // => true
+          cy.get('button').click();
+          cy.url().should('eq', 'http://localhost:8080/'); // => true
+        });
       });
     });
   });
